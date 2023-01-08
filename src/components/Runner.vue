0ca2471433d1c73db16355cc0034f238d110c06c
Amit Chambial <chambialamit@gmail.com>
2023-01-08 15:17:30 +0530

    added tabs

---
+++ src/components/Runner.vue
@@ -0,0 +1,143 @@
+<template>
+   <div  >
+        <div class="input" >
+            <el-form :model="form" :rules="rules" ref="ruleFormRef">
+                <div>
+                    <el-button type="primary" plain @click="submitForm" icon="VideoPlay">
+                        Run
+                    </el-button>
+                    <el-button type="primary" plain @click="cancelRequest" icon="Close">
+                        Cancel
+                    </el-button>
+                </div>
+                <br/>
+                <el-form-item prop="query" >
+                    <el-input type="textarea" v-model="form.query"/>
+                </el-form-item>
+                <el-form-item>
+                </el-form-item>
+            </el-form>
+           
+        </div>
+        <el-tabs v-model="activeTab" v-loading="loading" element-loading-text="fetching data">
+            <el-tab-pane label="Results" name="results" >
+                <div v-if="data" >
+                    <el-table :data="pagedTableData"  table-layout="auto" stripe border>
+                        <el-table-column in v-for="column in dataColumns" :key="column" :prop="column" :label="column"></el-table-column>
+                    </el-table>
+                    <el-pagination layout="prev, pager, next" :total="this.data.length" @current-change="setPage"></el-pagination>
+                </div>
+                <div v-else class="no-result">{{ resultMessage }}</div>
+            </el-tab-pane>
+            <el-tab-pane label="History" name="history">
+                <ul>
+                    <li v-for="(historyItem, index) in historyStore.history" :key="index">{{historyItem}}</li>
+                </ul>
+            </el-tab-pane>
+        </el-tabs>
+       
+    </div>
+</template>
+
+<script>
+import { useHistoryStore } from '../stores/history'
+import csvToJson from '../utils/csvToJson'
+export default {
+    setup(){
+    const historyStore = useHistoryStore()
+    return { historyStore }
+  },
+  created(){
+    this.controller = new AbortController();
+    this.signal = this.controller.signal;
+  },
+    data () {
+        return {
+            activeTab:'results',
+            form: {
+                query:'SELECT * FROM products'
+            },
+            currentPage:1,
+            pageSize:10,
+            rules:{
+                query: [
+                    { required: true, message: 'Please Enter some query to run', trigger: 'blur' },
+                    { min: 3, message: 'Length should be 3', trigger: 'blur' },
+                ],
+            },
+            data:null,
+            loading: false,
+            resultMessage:'Run the Query to see Results'
+        };
+    },
+    computed:{
+        dataColumns(){
+            return Object.keys(this.data[0])
+        },
+        pagedTableData() {
+            return this.data.slice(this.pageSize * this.currentPage - this.pageSize, this.pageSize * this.currentPage)
+        }
+    },
+    methods:{
+        cancelRequest(){
+            this.controller.abort()
+            this.controller = new AbortController();
+            this.signal = this.controller.signal;
+        },
+        submitForm(){
+            this.$refs['ruleFormRef'].validate(async (valid)=>{
+                if(!valid) return
+                this.loading = true
+                this.data=null
+                let payload = this.form.query.toLowerCase().split('select * from')[1].split(';')[0].trim()
+                try{
+                    await this.getData(payload)
+                    this.historyStore.addToHistory(this.form.query)
+                } catch(err){
+                    if (err.name === "AbortError") {
+                        this.resultMessage = 'Last Request was Aborted'
+                    } else alert('error occurred while fetching data')
+                } finally{
+                    this.loading = false
+                }
+            })
+        },
+        async getData(name) {
+            let data = await fetch (`https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/${name}.csv`, {signal:this.signal})
+            data  = await data.text()
+            data = csvToJson(data)
+           this.data = data
+        },
+        load() {
+            if(this.data.length<50) return
+            this.loading =true
+            setTimeout(()=>{
+                this.data.push(...this.data.slice(0,10))
+                this.loading =false
+            },3000)
+        },
+        setQuery(val) { 
+            this.form.query = `SELECT * FROM ${val}`
+        },
+        setPage(val){
+            this.currentPage =val
+        }
+    }
+}
+</script>
+
+<style>
+.input{
+    display: flex;
+    flex-grow: 1;
+}
+.input form{
+    flex: 1;
+}
+.input ul{
+    flex: 1;
+}
+.no-result{
+    min-width: 100px;
+}
+</style>
